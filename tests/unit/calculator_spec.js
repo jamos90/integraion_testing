6550da1acb45d1c5312e7dc7b0d43a3e600bde6f
jamos90 <jamos_98@hotmail.com>
2018-08-25 13:14:49 +0100

    test refactored to meet readme requirements

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -11,27 +11,28 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
-  it('should be able to add numbers to the running total', function(){
-    const actual = calculator.add(4);
-    assert.strictEqual(calculator.runningTotal,4);
+  it('should be able to add 1 to 4 to get 5', function(){
+    calculator.previousTotal = 4
+    const actual = calculator.add(1);
+    assert.strictEqual(calculator.runningTotal,5);
   })
 
-  it('should be able to remove 2 from 5 numbers and get 3', function(){
-    calculator.previousTotal = 5;
-    const actual = calculator.subtract(2);
+  it('should be able to remove 4 from 7 numbers and get 3', function(){
+    calculator.previousTotal = 7;
+    const actual = calculator.subtract(4);
     assert.strictEqual(calculator.runningTotal, 3);
   })
 
-  it('should be able to multiply 10 by 3 to get 30', function(){
-    calculator.previousTotal = 10;
-    const actual = calculator.multiply(3);
-    assert.strictEqual(calculator.runningTotal, 30);
+  it('should be able to multiply 3 by 5 to get 15', function(){
+    calculator.previousTotal = 3;
+    const actual = calculator.multiply(5);
+    assert.strictEqual(calculator.runningTotal, 15);
   })
 
-  it('should be ablit to divide 10 by 2 to get 5', function(){
-    calculator.previousTotal = 10;
-    const actual = calculator.divide(2);
-    assert.strictEqual(calculator.runningTotal, 5);
+  it('should be ablit to divide 21 by 7 to get 3', function(){
+    calculator.previousTotal = 21;
+    const actual = calculator.divide(7);
+    assert.strictEqual(calculator.runningTotal, 3);
   })
 
 });

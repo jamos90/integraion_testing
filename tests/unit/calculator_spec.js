d9d828512eea211cdec1404964cf3946b7f104d8
jamos90 <jamos_98@hotmail.com>
2018-08-25 12:51:52 +0100

    first unit test passing

---
+++ tests/unit/calculator_spec.js
@@ -0,0 +1,19 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  })
+
+  it('should be able to add numbers to the running total', function(){
+    const actual = calculator.add(4);
+    assert.strictEqual(calculator.runningTotal,4);
+  })
+
+});

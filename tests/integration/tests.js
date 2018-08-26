a2ef4ba1d7ac67c23bdea78d5d751182bbac21f4
jamos90 <jamos_98@hotmail.com>
2018-08-26 12:25:42 +0100

    multiple opertors tests passing

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -22,7 +22,28 @@ describe('calculator functionality', function() {
     element(by.css('#number4')).click();
     element(by.css('#number5')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('345')
+  })
 
+  it('should be able to update the display of a operation using the arithmetical operators', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number4')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('12');
+  })
+
+  it('should be able to chain multiple operations together', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('7');
   })
 
 });
